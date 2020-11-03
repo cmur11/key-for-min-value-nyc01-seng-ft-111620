b28bb11c3c227ab63e6605799438bcfd34971e61
Conor Murnane <conormurnane11@gmail.com>
2020-11-03 18:50:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
     name_hash.each do |key,value|
       #binding.pry
       if name_hash[value] < low_num
-        low_num = value
-       
+        binding.pry
+        ow_num = value
         low_name = key
         #binding.pry
       end
